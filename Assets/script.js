7b27771c906b89ce7a048416bf8d035ccbb69216
Hakkelo89 <78032311+Hakkelo89@users.noreply.github.com>
2021-04-07 21:25:40 +0100

    inital func's add and API key add

---
+++ Assets/script.js
@@ -0,0 +1,16 @@
+function initPage() {
+  const inputEl = document.getElementById("city-input");
+  const searchEl = document.getElementById("search-button");
+  const clearEl = document.getElementById("clear-history");
+  const nameEl = document.getElementById("city-name");
+  const currentPicEl = document.getElementById("current-pic");
+  const currentTempEl = document.getElementById("temperature");
+  const currentHumidityEl = document.getElementById("humidity");4
+  const currentWindEl = document.getElementById("wind-speed");
+  const currentUVEl = document.getElementById("UV-index");
+  const historyEl = document.getElementById("history");
+  let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
+  console.log(searchHistory);
+  
+
+  const APIKey = "fed8864c89b39699b771e2691aa4376a";
